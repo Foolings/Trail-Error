ee73099e0d12996dc1e1822c7854e8dbb0604025
Foolings <m.bolden2001@gmail.com>
2017-05-07 15:30:57 -0400

    update

--- boo.py
+++ boo.py
@@ -3,8 +3,8 @@ random_num = random.randint(1,10)
 print ("Hello, ladies and gentlemen! Welcome to Guess That Number!")
 print ("I'm Mettaton, your fabulous mechanical host!")
 print ("In this episode, we have a special guest with us.")
-name = input('')
-print ("Hello, %! Welcome to my show!") % name
+name = input()
+print ("Hello, {}! Welcome to my show!" .format(name))
 print ("Here are the rules:")
 print ("You guess a number between 1 and 10, selected randomly from the computer.")
 print ("There are three rounds.")
@@ -24,7 +24,7 @@ if guess==4:
 	print ("Onto the next round!")
 	print ("What's your next guess?")
 else:
-	print ("Sorry, that's incorrect. The number was %r.") % int(random_num)
+	print ("Sorry, that's incorrect. The number was {}." .format(random_num)) 
 	print ("You were so close, yet so far away!")
 	print ("That's it for tonight folks!")
 	print ("I'm Mettaton, and this has been...")
@@ -35,7 +35,7 @@ if guess2==4:
 	print ("Onto the next round!")
 	print ("What's your guess?")
 else:
-	print ("Sorry, that's incorrect. The number was %r.") % (random_num)
+	print ("Sorry, that's incorrect. The number was {}." .format(random_num))
 	print ("You were so close, yet so far away!")
 	print ("That's it for tonight folks!")
 	print ("I'm Mettaton, and this has been...")
@@ -45,7 +45,7 @@ if guess3==4:
 	print ("Congratulations! You are now entering the bonus round.")
 	print ("Do you want to participate?")
 else:
-	print ("Well, %r, you are taking home 15,000 dollars!") % (name)
+	print ("Well, {}, you are taking home 15,000 dollars!" .format(name))
 	print ("Congratulations!")
 	print ("That's it for tonight, folks!")
 	print ("I'm Mettaton, and this has been...")
@@ -56,13 +56,13 @@ if answer=='Yes':
 	print ("Last guess.")
 guess4 = int(input())
 if guess4==4:
-	print ("Congratulations, %r! You are this week's lucky winner!") % (name)
+	print ("Congratulations, {}! You are this week's lucky winner!" .format(name))
 	print ("You'll be taking home 25,000 dollars!")
 	print ("That's it for tonight, folks!")
 	print ("I'm Mettaton, and this has been...")
 	print ("GUESS THAT NUMBER!")
 else:
-	print ("Sorry, that's incorrect. The number was %r.") % (random_num)
+	print ("Sorry, that's incorrect. The number was {}." .format(random_num))
 	print ("You were so close, yet so far away!")
 	print ("That's it for tonight, folks!")
 	print ("I'm Mettaton, and this has been...")
